3a800d2492cbb282bbc0f3c53a1b207a4df73574
Anwesh43 <anweshthecool0@gmail.com>
2021-05-02 21:37:08 +0530

    adding fixes

--- src/hooks.ts
+++ src/hooks.ts
@@ -3,6 +3,10 @@ import {useState, useEffect, CSSProperties} from 'react'
 const scGap = 0.02 
 const delay = 20 
 
+const maxScale = (scale : number, i : number, n : number) : number => Math.max(0, scale - i / n) 
+
+const divideScale = (scale : number, i : number, n : number) : number => Math.min(1 / n, maxScale(scale, i, n)) * n 
+
 export const useAnimatedScale = () => {
     const [scale, setScale] = useState(0)
     const [animated, setAnimated] = useState(false)
@@ -50,10 +54,12 @@ export const useStyle = (w : number, h : number, scale : number) => {
     const size : number = Math.min(w, h) / 10 
     const position = 'absolute'
     const sf : number = Math.sin(scale * Math.PI)
+    const sf1 = divideScale(sf, 0, 2)
+    const sf2 = divideScale(sf, 1, 2)
     const left = `${0}px`
-    const top = `${(h - size) * (1 - sf)}px`
+    const top = `${(h - size) * (1 - sf1)}px`
     const width = `${size}px`
-    const height = `${size + (h - size) * sf}px`
+    const height = `${size + (h - size) * (sf1 - sf2)}px`
     const background = 'indigo'
     return {
         blockStyle() : CSSProperties {
